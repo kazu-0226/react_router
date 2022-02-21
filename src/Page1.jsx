82b26614d5cea34f9d3fb512821de2e4e22eab7e
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-02-21 15:01:21 +0900

    state

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
   return (
     <div>
       <h1>Page1です</h1>
-      <Link to="/page1/detailsA">DetailA</Link>
+      <Link to={{ pathname: "/page1/detailsA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailsB">DetailB</Link>
     </div>
--- src/Page1DetailsA.jsx
+++ src/Page1DetailsA.jsx
@@ -1,4 +1,8 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailsA = () => {
+  const { state } = useLocation();
+  console.log(state);
   return (
     <div>
       <h1>Page1DetailsA</h1>
