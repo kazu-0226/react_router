0e4e3e7bc2609998ceb32e0a310c2521da784849
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-02-21 12:30:24 +0900

    routesを切り出す

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailsA } from "./Page1DetailsA";
-import { Page1DetailsB } from "./Page1DetailsB";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
@@ -17,31 +13,7 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              {console.log(url)}
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route exact path={`${url}/detailsA`}>
-                <Page1DetailsA />
-              </Route>
-              <Route path={`${url}/detailsB`}>
-                <Page1DetailsB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
